368a459b04f83dbde7f3fbdb0d430a630834a220
Emmanuel Foeh <EmmanuelFo@stlghana.com>
2021-11-05 09:03:53 +0000

    working on home page intro section

--- src/components/Home page/Home.jsx
+++ src/components/Home page/Home.jsx
@@ -1,25 +1,54 @@
-import { Grid } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import React from 'react';
 
 import useStyle from './style';
 const Home = () => {
 	const classes = useStyle();
 	return (
-    <Grid container className={classes.home}>
-		the home component
-      {/* <Grid item xs={12} md={8}>
-        first section
-      </Grid>
-      <Grid item xs={12} md={4}>
-        2nd section
-      </Grid>
-      <Grid item xs={12} md={4}>
-        third section
-      </Grid>
-      <Grid item xs={12} md={8}>
-        fouth section
-      </Grid> */}
-    </Grid>
+    <main className={classes.home}>
+      <section className={classes.intro_section}>
+        <div className={classes.intro_section_text}>
+          <div className={classes.logo_div}>
+            <span className={classes.logo}>
+              <img src="img\icon-ambulance.svg" alt="logo" />
+            </span>
+
+            <Typography display="inline" variant="h5">
+              NEED AMBULANCE?
+            </Typography>
+          </div>
+          <div>
+             <Typography fontSize="5rem" variant="1">STAY HOME, STAY SAFE!</Typography>
+          <p>
+            Purchase medication from legitimate websites online. Search for a
+            site to see if it is safe or not recommended. Join 8,405 of happy
+            customers from the USA who give us a 99% satisfaction rate. They
+            talk about
+          </p>
+          <Button color="primary">UPLOAD PRESCRIPTION</Button>
+          </div>
+         
+        </div>
+        <div>second section</div>
+      </section>
+
+      <section className={classes.services}>
+        thi is the services section
+      </section>
+      <section className={classes.medicine}>The medicine section</section>
+      <section className={classes.about}>this is the about section</section>
+
+      <section className={classes.medicine_serve}>
+        medicine serve section
+      </section>
+      <section className={classes.testimonial}>
+        this is the testimonial section
+      </section>
+
+      <section className={classes.subscribe}>get lastest news section</section>
+
+      <footer className={classes.footer}>this is the footer section</footer>
+    </main>
   );
 };
 
--- src/components/Home page/style.js
+++ src/components/Home page/style.js
@@ -3,6 +3,18 @@ import { makeStyles } from "@mui/styles";
 export default makeStyles((theme)=>({
 home:{
     height:'calc(100vh - 64px)',
-    backgroundColor:'bisque',
-}
+    // backgroundColor:'bisque',
+},
+intro_section:{
+    maxWidth:'83.3125rem',
+    display:'grid',
+    gridTemplateColumns:'1fr 1fr',
+    // backgroundColor:'green',
+    padding:'1rem 1rem',
+},
+text_section_text:{},
+logo_div:{
+    display:'flex',
+    "& span":{marginRight:"1rem"},
+},
 }));
